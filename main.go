e1cc56302e3ddc8815ac5db2a956effc3473cb82
Momoh Nobert <okhai0112@gmail.com>
2024-02-21 10:51:47 +0100

    lecture 6 - pools

--- main.go
+++ main.go
@@ -6,47 +6,28 @@ import (
 	"sync/atomic"
 )
 
-type Person struct {
-	Name string
-	Age  int32
+type DBConnection struct {
+	index int64
 }
 
 func main() {
-	// option 1
-	// var count int32
-
-	// option 2
-	// var count atomic.Int32
-	var waitGroup sync.WaitGroup
-	var person atomic.Value
-
-	person.Store(&Person{Name: "Nobert", Age: 28})
+	var count int64
+	connectionPool := &sync.Pool{
+		New: func() any {
+			atomic.AddInt64(&count, 1)
+			return &DBConnection{
+				index: count,
+			}
+		},
+	}
 
 	for i := 0; i < 10; i++ {
-		waitGroup.Add(1)
 		go func() {
-			defer waitGroup.Done()
-			// option 1
-			// atomic.AddInt32(&count, 1)
-
-			// option 2
-			// count.Add(1)
-
-			// using values, load person into variable
-			// atomic operations? pointers!
-			// .(*Person), cast to person.
-			altPerson := person.Load().(*Person)
-			atomic.AddInt32(&altPerson.Age, 1)
+			dbConn := connectionPool.Get().(*DBConnection)
+			fmt.Printf("db connection: %v\n", dbConn)
+			connectionPool.Put(dbConn)
 		}()
 	}
 
-	waitGroup.Wait()
-	// option 1
-	// fmt.Println(atomic.LoadInt32(&count))
-
-	// option 2
-	// fmt.Println(count.Load())
-
-	// use atomic operations, load atomic values & cast
-	fmt.Println(person.Load().(*Person))
+	fmt.Scanln()
 }
