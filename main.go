bdac4c282f188ed429969176f77a010a3ed241c8
Momoh Nobert <okhai0112@gmail.com>
2024-02-21 17:11:39 +0100

    lecture 8 - select

--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	// add size (1) to buffer.
-	channel := make(chan string, 1)
+	channel1 := make(chan string)
+	channel2 := make(chan string)
 
-	// send a message to channel.
-	// blocking thread.
-	// in this case, main thread.
-	channel <- "message"
+	go func() {
+		channel1 <- "message to first channel"
+	}()
 
-	// ? add another message
-	// ! channel is full
-	channel <- "another message"
+	go func() {
+		channel2 <- "message to second channel"
+	}()
 
-	fmt.Println(<-channel)
+	// additional goroutines for each read operation
+	// go func() {
+	// 	for value := range channel1 {
+	// 		fmt.Println(value)
+	// 	}
+	// }()
+
+	// go func() {
+	// 	for value := range channel2 {
+	// 		fmt.Println(value)
+	// 	}
+	// }()
+
+	// select statement
+	timeout := time.After(5 * time.Second)
+	go func() {
+		for {
+			select {
+			case value := <-channel1:
+				fmt.Println(value)
+			case value := <-channel2:
+				fmt.Println(value)
+			case <-timeout:
+				fmt.Println("5 seconds without messages")
+				panic("no messages")
+			default:
+				time.Sleep(time.Second)
+				fmt.Println("waiting...")
+			}
+		}
+	}()
+
+	fmt.Scanln()
 }
