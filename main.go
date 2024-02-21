eb4d9b37b19c1b4b172da1880614951d4671ef00
Momoh Nobert <okhai0112@gmail.com>
2024-02-21 15:50:53 +0100

    lecture 7 - ex 3 - channels

--- main.go
+++ main.go
@@ -6,31 +6,13 @@ import (
 )
 
 func main() {
-	channel := make(chan string)
+	done := make(chan bool)
 
-	go sender(channel)
-	go receiver(channel)
+	go func() {
+		time.Sleep(2 * time.Second)
+		done <- true
+	}()
 
-	fmt.Scanln()
-}
-
-// chan<- in the function implies send only channel.
-// ! if we try to read, receive an error
-// assure it will only be used to send messages
-func sender(channel chan<- string) {
-	for i := 0; i < 100; i++ {
-		time.Sleep(time.Second)
-		channel <- fmt.Sprintf("countdown for %v seconds", i)
-	}
-}
-
-// chan<- in the function implies receive only channel.
-// ! if we try to send, receive an error
-// assure it will only be used to receive messages
-func receiver(channel <-chan string) {
-	// loop through using for statement.
-	// with range as channel.
-	for message := range channel {
-		fmt.Println(message)
-	}
+	<-done
+	fmt.Printf("main thread unblocked")
 }
